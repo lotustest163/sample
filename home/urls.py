fc6dd4f64f261b5aee18a5b51aa864889b23aaab
You Zhou <zhouyou.xy@gmail.com>
2016-11-10 15:47:34 +1100

    copied target homepage to base.html

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
 ]
